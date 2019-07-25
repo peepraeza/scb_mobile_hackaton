cfa1a83c4a5001ccedcdd1de830efc06330661dd
Nattakit Nganrungrueang <nattakit.nganrungrueang@scb.co.th>
2019-07-25 15:00:43 +0700

    rewordpage

--- app/src/main/java/com/example/myapplication/RewardPage.kt
+++ app/src/main/java/com/example/myapplication/RewardPage.kt
@@ -1,12 +1,30 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.models.ProfileParcel
+import kotlinx.android.synthetic.main.home_page.*
 
 class RewardPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reward_page)
+        val userProfile = intent.getParcelableExtra<ProfileParcel>("userProfile")
+        val(id, name, lastName, phoneNumber, gender, alias, point)= userProfile
+//
+        name_homepage_text.setText("${name} ${lastName}" )
+        point_txt.setText(point)
+
+        reward_btn.setOnClickListener{
+            val intent = Intent(this@RewardPage, RewardPage::class.java)
+            startActivity(intent)
+        }
+
+        cardless_btn.setOnClickListener{
+            val intent = Intent(this@RewardPage, Details::class.java)
+            startActivity(intent)
+        }
     }
 }
